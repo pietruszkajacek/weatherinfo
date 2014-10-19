a74b635a41293f3a3398e431e45a3a5becbdedb6
Jacek Pietruszka <pietruszka.jacek@gmail.com>
2014-10-20 00:56:25 +0200

    Fixed once again

--- application/controllers/Administration.php
+++ application/controllers/Administration.php
@@ -1,25 +1,38 @@
 <?php
 
-class Administration extends MY_Controller {
-	
-	 public function __construct()
-	 {
-		 parent::__construct();
-		 
-		 $this->load->model('weatherinfo_model');
-	 }
-	 
-	public function frontendcities() 
+class Administration extends MY_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->model('weatherinfo_model');
+	}
+
+	public function frontendcities()
 	{
 		if (!$this->ion_auth->logged_in())
 		{
-			//redirect them to the login page
-			redirect('administration/login', 'refresh');
+			if ($this->input->is_ajax_request())
+			{
+				$this->output->set_status_header('403');
+			}
+			else
+			{
+				redirect('administration/login', 'refresh');
+			}
 		}
-		elseif (!$this->ion_auth->is_admin()) 
+		elseif (!$this->ion_auth->is_admin())
 		{
-			$this->session->set_flashdata(array('type' => 'info', 'You must be an administrator to view this page.'));
-			redirect('/', 'refresh');
+			if ($this->input->is_ajax_request())
+			{
+				$this->output->set_status_header('403');
+			}
+			else
+			{
+				$this->session->set_flashdata(array('type' => 'info', 'You must be an administrator to view this page.'));
+				redirect('/', 'refresh');
+			}
 		}
 		else
 		{
@@ -31,9 +44,9 @@ class Administration extends MY_Controller {
 				}
 				else
 				{
-					$status = 0;
+					$status = -1;
 				}
-				
+
 				$this->output
 						->set_content_type('application/json')
 						->set_output(json_encode(array("status" => $status)));
@@ -41,32 +54,32 @@ class Administration extends MY_Controller {
 			else
 			{
 				$this->data['form_attr'] = array(
-					'class' => 'form-settings',
-					'id' => 'form-settings',
-					'role' => 'form'
+					'class'	 => 'form-settings',
+					'id'	 => 'form-settings',
+					'role'	 => 'form'
 				);
 
 				$this->data['country_name'] = array(
-					'id' => 'country-name',
-					'class' => 'form-control',
-					'type' => 'text',
-					'placeholder' => 'Enter country name',
+					'id'			 => 'country-name',
+					'class'			 => 'form-control',
+					'type'			 => 'text',
+					'placeholder'	 => 'Enter country name',
 				);
 
 				$this->data['country_name_label'] = array(
-					'for' => 'country_name',
-					'text' => 'Country name',
+					'for'		 => 'country_name',
+					'text'		 => 'Country name',
 					'attributes' => array(
 					),
 				);
-				
+
 				$this->data['front_end_cities'] = $this->weatherinfo_model->get_front_end_cities();
-				
+
 				$this->render();
 			}
-		}		
+		}
 	}
-	
+
 	public function provider()
 	{
 		if (!$this->ion_auth->logged_in())
@@ -74,65 +87,65 @@ class Administration extends MY_Controller {
 			//redirect them to the login page
 			redirect('administration/login', 'refresh');
 		}
-		elseif (!$this->ion_auth->is_admin()) 
+		elseif (!$this->ion_auth->is_admin())
 		{
 			$this->session->set_flashdata(array('type' => 'info', 'You must be an administrator to view this page.'));
 			redirect('/', 'refresh');
 		}
 		else
-		{	
+		{
 			$this->form_validation->set_rules('wsdl', 'Wsdl', 'required');
 			$this->form_validation->set_rules('con_timeout', 'Connection timeout', 'required|is_natural_no_zero');
-			
+
 			if ($this->form_validation->run() == TRUE)
 			{
 				$this->weatherinfo_model->update_weather_provider(1, $this->input->post('wsdl'), $this->input->post('con_timeout'));
-				
+
 				$this->session->set_flashdata(array('type' => 'info', 'msg' => 'Submit successfully'));
-				redirect("administration/provider", 'refresh');		
+				redirect("administration/provider", 'refresh');
 			}
-			
+
 			$weather_provider = $this->weatherinfo_model->get_weather_provider(1);
 
 			$this->data['message'] = array(
-				'type' => $this->session->flashdata('type'),
-				'msg' => $this->session->flashdata('msg'),
+				'type'	 => $this->session->flashdata('type'),
+				'msg'	 => $this->session->flashdata('msg'),
 			);
 
 			$this->data['form_attr'] = array(
-				'class' => 'form-settings',
-				'id' => 'form-settings',
-				'role' => 'form'
+				'class'	 => 'form-settings',
+				'id'	 => 'form-settings',
+				'role'	 => 'form'
 			);
 
 			$this->data['wsdl'] = array(
-				'name' => 'wsdl',
-				'id' => 'wsdl',
-				'class' => 'form-control',
-				'type' => 'text',
-				'placeholder' => 'Enter WSDL',
-				'value' => $this->form_validation->set_value('wsdl', ! is_null($weather_provider) ? $weather_provider->wsdl : ''),
+				'name'			 => 'wsdl',
+				'id'			 => 'wsdl',
+				'class'			 => 'form-control',
+				'type'			 => 'text',
+				'placeholder'	 => 'Enter WSDL',
+				'value'			 => $this->form_validation->set_value('wsdl', !is_null($weather_provider) ? $weather_provider->wsdl : ''),
 			);
 
 			$this->data['wsdl_label'] = array(
-				'for' => 'wsdl',
-				'text' => 'WSDL',
+				'for'		 => 'wsdl',
+				'text'		 => 'WSDL',
 				'attributes' => array(
 				),
 			);
 
 			$this->data['connection_timeout'] = array(
-				'name' => 'con_timeout',
-				'id' => 'con_timeout',
-				'class' => 'form-control',
-				'type' => 'text',
-				'placeholder' => 'Enter connection timeout in sec.',
-				'value' => $this->form_validation->set_value('con_timeout', ! is_null($weather_provider) ? $weather_provider->connection_timeout : ''),
+				'name'			 => 'con_timeout',
+				'id'			 => 'con_timeout',
+				'class'			 => 'form-control',
+				'type'			 => 'text',
+				'placeholder'	 => 'Enter connection timeout in sec.',
+				'value'			 => $this->form_validation->set_value('con_timeout', !is_null($weather_provider) ? $weather_provider->connection_timeout : ''),
 			);
 
 			$this->data['connection_timeout_label'] = array(
-				'for' => 'con_timeout',
-				'text' => 'SOAP connection timeout',
+				'for'		 => 'con_timeout',
+				'text'		 => 'SOAP connection timeout',
 				'attributes' => array(
 				),
 			);
@@ -141,33 +154,33 @@ class Administration extends MY_Controller {
 		}
 	}
 
-	public function getcitiesbycountry() 
+	public function getcitiesbycountry()
 	{
 		if ($this->input->is_ajax_request())
 		{
 			if ($this->ion_auth->logged_in())
 			{
 				$weather_provider = $this->weatherinfo_model->get_weather_provider(1);
-				
+
 				$wsdl = $weather_provider->wsdl;
 				$connection_timeout = intval($weather_provider->connection_timeout);
-				
-				try 
-				{ 
+
+				try
+				{
 					$result = array();
-					
-					$globalprovider = new My\GlobalWeatherProvider\Globalweather_weather_provider($wsdl, $connection_timeout);					
+
+					$globalprovider = new My\GlobalWeatherProvider\Globalweather_weather_provider($wsdl, $connection_timeout);
 					$this->data['cities'] = $globalprovider->get_cities_by_country($this->input->post('country'));
-					
+
 					$result['status'] = 1;
 					$result['cities'] = $this->load->view('/partials/cities_by_country', $this->data, TRUE);
 				}
-				catch (Exception $e) 
-				{ 
-				    $result['status'] = -1;
+				catch (Exception $e)
+				{
+					$result['status'] = -1;
 					$result['msg'] = $e->getMessage();
-				} 
-				
+				}
+
 				$this->output
 						->set_content_type('application/json')
 						->set_output(json_encode($result)
@@ -175,11 +188,11 @@ class Administration extends MY_Controller {
 			}
 			else
 			{
-                $this->output->set_status_header('403');
+				$this->output->set_status_header('403');
 			}
 		}
 	}
-	
+
 	public function login()
 	{
 		if ($this->ion_auth->logged_in())
@@ -217,38 +230,38 @@ class Administration extends MY_Controller {
 		else
 		{
 			$this->data['message'] = array(
-				'type' => $this->session->flashdata('type'),
-				'msg' => $this->session->flashdata('msg'),
+				'type'	 => $this->session->flashdata('type'),
+				'msg'	 => $this->session->flashdata('msg'),
 			);
 
 			$this->data['form_attr'] = array(
-				'class' => 'form-signin',
-				'id' => 'form-signin',
-				'role' => 'form'
+				'class'	 => 'form-signin',
+				'id'	 => 'form-signin',
+				'role'	 => 'form'
 			);
 
 			$this->data['email'] = array(
-				'type' => 'email',
-				'name' => 'email',
-				'id' => 'email',
-				'class' => 'form-control',
-				'placeholder' => 'Email address',
-				'value' => $this->form_validation->set_value('email'),
+				'type'			 => 'email',
+				'name'			 => 'email',
+				'id'			 => 'email',
+				'class'			 => 'form-control',
+				'placeholder'	 => 'Email address',
+				'value'			 => $this->form_validation->set_value('email'),
 			);
 
 			$this->data['password'] = array(
-				'type' => 'password',
-				'name' => 'password',
-				'id' => 'password',
-				'class' => 'form-control',
-				'placeholder' => 'Password'
+				'type'			 => 'password',
+				'name'			 => 'password',
+				'id'			 => 'password',
+				'class'			 => 'form-control',
+				'placeholder'	 => 'Password'
 			);
 
 			$remember = array(
-				'name' => 'remember_me',
-				'id' => 'remember_me',
-				'value' => '1',
-				'checked' => (bool) $this->input->post('remember_me') ? TRUE : FALSE,
+				'name'		 => 'remember_me',
+				'id'		 => 'remember_me',
+				'value'		 => '1',
+				'checked'	 => (bool) $this->input->post('remember_me') ? TRUE : FALSE,
 			);
 
 			$remember_me_checkbox = form_checkbox($remember);
@@ -258,16 +271,16 @@ class Administration extends MY_Controller {
 			);
 
 			$this->data['form_groups'] = array(
-				'email' => form_error('email') ? ' has-error' : '',
-				'password' => form_error('password') ? ' has-error' : '',
+				'email'		 => form_error('email') ? ' has-error' : '',
+				'password'	 => form_error('password') ? ' has-error' : '',
 			);
-			
+
 			$this->has_nav_top_bar = FALSE;
-			
+
 			$this->render();
 		}
 	}
-	
+
 	public function logout()
 	{
 		$this->data['title'] = "Logout";
@@ -277,8 +290,9 @@ class Administration extends MY_Controller {
 
 		//redirect them to the home page
 		$this->session->set_flashdata(array('type' => 'info', 'msg' => $this->ion_auth->messages()));
-		redirect('/', 'refresh');		
+		redirect('/', 'refresh');
 	}
+
 }
 
 /* End of file Administration.php */
--- assets/js/administration.js
+++ assets/js/administration.js
@@ -1,55 +1,54 @@
 WINFO = $.extend(true, (typeof WINFO === 'undefined') ? {} : WINFO, {
-	
 	administration: {
 		start_drag: false,
-		sortable_receive:  false,
+		sortable_receive: false,
 		disable_city_allowed: false,
 		remove_city_allowed: false,
-		
 		get_cities_requested: false,
+		front_end_cities_submit: false,
 		
 		index: function () {
 
 		},
 		
 		frontendcities: function () {
-				
-			$( '#get-cities-btn' ).click( WINFO.administration._get_cities_by_country );
-			$( '#form-settings' ).submit( WINFO.administration._submit );
-			$( '#country-name' ).keypress( function ( event ) {
-				if ( event.which == 13 ) {
+
+			$('#get-cities-btn').click(WINFO.administration._get_cities_by_country);
+			$('#form-settings').submit(WINFO.administration._submit);
+			$('#country-name').keypress(function (event) {
+				if (event.which == 13) {
 					event.preventDefault();
-					if ( ! WINFO.administration.get_cities_requested )
-						WINFO.administration._get_cities_by_country( event );
+					if (!WINFO.administration.get_cities_requested)
+						WINFO.administration._get_cities_by_country(event);
 				}
-			} );
-			
+			});
+
 			$("#front-end-cities ul").sortable({
-				receive: function ( event, ui )
+				receive: function (event, ui)
 				{
 					//console.log('receive');
-					if ( WINFO.administration.start_drag )
+					if (WINFO.administration.start_drag)
 					{
 						WINFO.administration.sortable_receive = true;
 					}
 				},
-				beforeStop: function ( event, ui )
+				beforeStop: function (event, ui)
 				{
 					//console.log('beforestop');
-					if ( WINFO.administration.remove_city_allowed )
+					if (WINFO.administration.remove_city_allowed)
 					{
 						WINFO.administration.remove_city_allowed = false;
-						$( "#front-end-cities ul" ).sortable( "enable" );
-						$( "li[data-country='" + $(ui.item).attr( 'data-country' ).replace(/\'/g, "\\'") + "'][data-city='" 
-								+ $(ui.item).attr( 'data-city' ).replace(/\'/g, "\\'") + "']", $( '#cities' ) ).draggable( 'enable' );
+						$("#front-end-cities ul").sortable("enable");
+						$("li[data-country='" + $(ui.item).attr('data-country').replace(/\'/g, "\\'") + "'][data-city='"
+								+ $(ui.item).attr('data-city').replace(/\'/g, "\\'") + "']", $('#cities')).draggable('enable');
 					}
 				},
-				out: function ( event, ui )
+				out: function (event, ui)
 				{
 					//console.log('out');
-					if ( WINFO.administration.start_drag )
+					if (WINFO.administration.start_drag)
 					{
-						if ( WINFO.administration.sortable_receive )
+						if (WINFO.administration.sortable_receive)
 						{
 							WINFO.administration.disable_city_allowed = true;
 							WINFO.administration.sortable_receive = false;
@@ -60,178 +59,206 @@ WINFO = $.extend(true, (typeof WINFO === 'undefined') ? {} : WINFO, {
 						}
 					}
 				}
-			} ),
-			
-			$( "#cities" ).droppable( {
+			}),
+					$("#cities").droppable({
 				accept: "#front-end-cities li",
-				drop: function ( event, ui ) {
+				drop: function (event, ui) {
 					//console.log('drop');
-					$( "#front-end-cities ul" ).sortable( "disable" );
+					$("#front-end-cities ul").sortable("disable");
 					(ui.helper).remove();
 					WINFO.administration.remove_city_allowed = true;
 				}
-			} );
-			
-			$("ul, li").disableSelection();			
+			});
+
+			$("ul, li").disableSelection();
 		},
-	
-		_get_cities_by_country: function ( event ) {
+		_get_cities_by_country: function (event) {
 			event.preventDefault();
 			var country = $('#country-name').val();
-			
-			if (country === "") return;
-			
-			if ( ! WINFO.administration.get_cities_requested ) {
-				
+
+			if (country === "")
+				return;
+
+			if (!WINFO.administration.get_cities_requested) {
+
 				WINFO.administration.get_cities_requested = true;
-				
-				$( '#get-cities-btn' ).addClass( 'disabled' );
+
+				$('#get-cities-btn').addClass('disabled');
 				WINFO.common.clean_alerts();
 
-				$.ajax( {
+				$.ajax({
 					cache: false,
 					url: '/administration/getcitiesbycountry/',
 					type: 'POST',
 					data: {country: country},
-					success: function ( data ) {
-						if ( data.status == 1 ) {
+					success: function (data) {
+						if (data.status == 1) {
 
-							$( '#cities' ).scrollTop( 0 );
+							$('#cities').scrollTop(0);
 
 							//add city to #cities
-							$( '#cities ul' ).html( data.cities );
+							$('#cities ul').html(data.cities);
 
-							$( "#cities ul > li" ).draggable( {
+							$("#cities ul > li").draggable({
 								connectToSortable: "#front-end-cities ul",
 								helper: "clone",
-								start: function ( event, ui )
+								start: function (event, ui)
 								{
 									//console.log('start_drag');
 									WINFO.administration.start_drag = true;
 								},
-								stop: function ( event, ui )
+								stop: function (event, ui)
 								{
 									//console.log('stop_drag');
 									WINFO.administration.start_drag = false;
 
-									if ( WINFO.administration.disable_city_allowed )
+									if (WINFO.administration.disable_city_allowed)
 									{
-										$( this ).draggable( "disable" );
+										$(this).draggable("disable");
 										WINFO.administration.disable_city_allowed = false;
 									}
 								}
-							} );
+							});
 
-							$( '#cities li' ).each( function () {
-								$item = $( this );
+							$('#cities li').each(function () {
+								$item = $(this);
 
 								//alert("li[data-country='" + $item.attr( 'data-country' ) + "'][data-city='" + $item.attr( 'data-city' ) + "']");
 
-								if ( $( "li[data-country='" + $item.attr( 'data-country' ).replace( /\'/g, "\\'" ) + "'][data-city='"
-										+ $item.attr( 'data-city' ).replace( /\'/g, "\\'" ) + "']", $( '#front-end-cities' ) ).length !== 0 )
+								if ($("li[data-country='" + $item.attr('data-country').replace(/\'/g, "\\'") + "'][data-city='"
+										+ $item.attr('data-city').replace(/\'/g, "\\'") + "']", $('#front-end-cities')).length !== 0)
 								{
-									$item.draggable( 'disable' );
+									$item.draggable('disable');
 								}
-							} );
+							});
 						}
-						else if ( data.status == - 1 ) {
+						else if (data.status == -1) {
 
-							WINFO.common.alert( '#main', 'Error!', data.msg, 'danger' );
+							WINFO.common.alert('#main', 'Error!', data.msg, 'danger');
 						}
-						
-						$( '#get-cities-btn' ).removeClass( 'disabled' );
+
+						$('#get-cities-btn').removeClass('disabled');
 						WINFO.administration.get_cities_requested = false;
 					},
-					error: function ( jqXHR, textStatus, errorThrown ) {
-						WINFO.common.alert( '#main', textStatus, errorThrown, 'danger' );
-						
-						$( '#get-cities-btn' ).removeClass( 'disabled' );
+					error: function (jqXHR, textStatus, errorThrown) {
+						WINFO.common.alert('#main', textStatus, errorThrown, 'danger');
+
+						$('#get-cities-btn').removeClass('disabled');
 						WINFO.administration.get_cities_requested = false;
 					}
 
-				} );
+				});
 			}
 		},
-	
 		provider: function () {
 
-			$( '#form-settings' ).submit( function () {
+			$('#form-settings').submit(function () {
 
 				//var number_front_end_cities = $("#front-end-cities li").length;
 
 				var validation_errors = '';
 
-				if ( $( 'input[name="wsdl"]' ).val() == "" ) {
+				if ($('input[name="wsdl"]').val() == "") {
 
-					$( 'input[name="wsdl"]' ).parent().addClass( 'has-error' );
+					$('input[name="wsdl"]').parent().addClass('has-error');
 
 					validation_errors = validation_errors + 'Enter WSDL address.' + '<br />';
 
 				} else {
 
-					$( 'input[name="wsdl"]' ).parent().removeClass( 'has-error' );
+					$('input[name="wsdl"]').parent().removeClass('has-error');
 
 				}
 
-				if ( $( 'input[name="con_timeout"]' ).val() == "" ) {
+				if ($('input[name="con_timeout"]').val() == "") {
 
-					$( 'input[name="con_timeout"]' ).parent().addClass( 'has-error' );
+					$('input[name="con_timeout"]').parent().addClass('has-error');
 					validation_errors = validation_errors + 'Enter connection timeout in sec.' + '<br />';
 
 				} else {
 
-					if ( ! /^(0?[1-9]\d*)$/.test( $( 'input[name="con_timeout"]' ).val() ) ) {
+					if (!/^(0?[1-9]\d*)$/.test($('input[name="con_timeout"]').val())) {
 
-						$( 'input[name="con_timeout"]' ).parent().addClass( 'has-error' );
+						$('input[name="con_timeout"]').parent().addClass('has-error');
 						validation_errors = validation_errors + 'Connection timeout must by positive integer.' + '<br />';
 
 					} else {
 
-						$( 'input[name="con_timeout"]' ).parent().removeClass( 'has-error' );
+						$('input[name="con_timeout"]').parent().removeClass('has-error');
 
 					}
 				}
 
-				if ( validation_errors == '' ) {
-					
+				if (validation_errors == '') {
+
 					return true;
-					
+
 				} else {
-					
+
 					WINFO.common.clean_alerts();
-					WINFO.common.alert( '#main', 'Error!', validation_errors, 'danger' );
-					
+					WINFO.common.alert('#main', 'Error!', validation_errors, 'danger');
+
 					return false;
 				}
-			} );
+			});
 		},
-		
-		_submit: function ( event ) {
+		_submit: function (event) {
 
 			/* stop form from submitting normally */
 			event.preventDefault();
 
-			WINFO.common.clean_alerts();
+			if (!WINFO.administration.front_end_cities_submit) {
 
-			$( "#front-end-cities li" ).each( function ( i, val ) {
-				var $city_li_item = $( this );
+				WINFO.administration.front_end_cities_submit = true;
+				$('#submit-btn').addClass('disabled');
 
-				$( 'input:hidden', $city_li_item ).eq( 0 ).attr( 'name', 'front-end-cities[' + i + '][country]' );
-				$( 'input:hidden', $city_li_item ).eq( 1 ).attr( 'name', 'front-end-cities[' + i + '][city]' );
-			} );
+				WINFO.common.clean_alerts();
 
-			var $form = $( this ),
-					url = $form.attr( 'action' );
+				$("#front-end-cities li").each(function (i, val) {
+					var $city_li_item = $(this);
 
-			$.post( url, $form.serialize(), function ( data ) {
-				if ( data.status == 1 ) {
-					WINFO.common.alert( '#main', 'Info!', 'Submit successfully', 'info' );
-				}
-				else if ( data.status == - 1 ) {
-					WINFO.common.alert( '#main', 'Error!', 'Submit error', 'danger' );
-				}
-			}, "json" );
+					$('input:hidden', $city_li_item).eq(0).attr('name', 'front-end-cities[' + i + '][country]');
+					$('input:hidden', $city_li_item).eq(1).attr('name', 'front-end-cities[' + i + '][city]');
+				});
 
+				var $form = $(this),
+						url = $form.attr('action');
+
+				$.ajax({
+					cache: false,
+					url: url,
+					type: 'POST',
+					data: $form.serialize(),
+					success: function (data) {
+						if (data.status == 1) {
+							WINFO.common.alert('#main', 'Info!', 'Submit successfully', 'info');
+						}
+						else if (data.status == -1) {
+							WINFO.common.alert('#main', 'Error!', 'Submit error', 'danger');
+						}
+						
+						WINFO.administration.front_end_cities_submit = false;
+						$('#submit-btn').removeClass('disabled');
+					},
+					error: function (jqXHR, textStatus, errorThrown) {
+						WINFO.common.alert('#main', textStatus, errorThrown, 'danger');
+						
+						WINFO.administration.front_end_cities_submit = false;
+						$('#submit-btn').removeClass('disabled');
+					}
+				});
+			}
+//			$.post(url, $form.serialize(), function (data) {
+//				if (data.status == 1) {
+//					WINFO.common.alert('#main', 'Info!', 'Submit successfully', 'info');
+//				}
+//				else if (data.status == -1) {
+//					WINFO.common.alert('#main', 'Error!', 'Submit error', 'danger');
+//				}
+//			}, "json")
+//			.fail(function () {
+//				alert("error");
+//			});
 		}
 	}
-} );
+});
